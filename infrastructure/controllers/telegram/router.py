f02468dc26cfac4d500cf58ff03a1e752cc17546
Kgnot <hricaurtem@proton.me>
2026-01-24 07:23:38 -0500

    agrego un comentario

--- infrastructure/controllers/telegram/router.py
+++ infrastructure/controllers/telegram/router.py
@@ -7,6 +7,7 @@ from infrastructure.mapper import telegram_to_domain_message, telegram_user_mapp
 telegram_router = Router()
 conversationServiceController = ConversationServiceController()
 
+# Esto se usa en: TelegramBotServices. Le decimos al dispatcher que routee la respuesta de esta forma
 @telegram_router.message()
 async def handle_message(message: Message):
     user = telegram_user_mapper(message)
